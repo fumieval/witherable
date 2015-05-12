dc5fc986b8d19913233a8934ad6acc6cccc57615
Fumiaki Kinoshita <fumiexcel@gmail.com>
2015-05-12 14:26:26 +0900

    Add generalized combinators

--- src/Data/Witherable.hs
+++ src/Data/Witherable.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE CPP, DeriveFunctor, DeriveFoldable, DeriveTraversable, StandaloneDeriving, UndecidableInstances, FlexibleContexts #-}
 -----------------------------------------------------------------------------
 -- |
@@ -33,6 +34,35 @@ import Data.Orphans ()
 import Data.Proxy
 #endif
 
+type FilterLike f s t a b = (a -> f (Maybe b)) -> s -> f t
+type Filter s t a b = forall f. Applicative f => FilterLike f s t a b
+type FilterLike' f s a = FilterLike f s s a a
+type Filter' s a = forall f. Applicative f => FilterLike' f s a
+
+-- | 'witherOf' is actually 'id', but left for consistency.
+witherOf :: FilterLike f s t a b -> (a -> f (Maybe b)) -> s -> f t
+witherOf = id
+{-# INLINE witherOf #-}
+
+-- | 'mapMaybe' through a filter.
+mapMaybeOf :: FilterLike Identity s t a b -> (a -> Maybe b) -> s -> t
+mapMaybeOf w f = runIdentity . w (Identity . f)
+{-# INLINE mapMaybeOf #-}
+
+-- | 'catMaybes' through a filter.
+catMaybesOf :: FilterLike Identity s t (Maybe a) a -> s -> t
+catMaybesOf w = mapMaybeOf w id
+{-# INLINE catMaybesOf #-}
+
+filterAOf :: Functor f => FilterLike' f s a -> (a -> f Bool) -> s -> f s
+filterAOf w f = w $ \a -> (\b -> if b then Just a else Nothing) <$> f a
+{-# INLINABLE filterAOf #-}
+
+-- | Filter each element of a structure targeted by a 'Filter'.
+filterOf :: FilterLike' Identity s a -> (a -> Bool) -> s -> s
+filterOf w f = runIdentity . filterAOf w (Identity . f)
+{-# INLINE filterOf #-}
+
 -- | Like `traverse`, but you can remove elements instead of updating them.
 --
 -- @'traverse' f ≡ 'wither' ('fmap' 'Just' . f)@
@@ -54,20 +84,21 @@ class T.Traversable t => Witherable t where
 
   wither :: Applicative f => (a -> f (Maybe b)) -> t a -> f (t b)
   wither f = fmap catMaybes . T.traverse f
+  {-# INLINE wither #-}
 
   mapMaybe :: (a -> Maybe b) -> t a -> t b
-  mapMaybe f = runIdentity . wither (Identity . f)
+  mapMaybe = mapMaybeOf wither
   {-# INLINE mapMaybe #-}
 
   catMaybes :: t (Maybe a) -> t a
-  catMaybes = mapMaybe id
+  catMaybes = catMaybesOf wither
   {-# INLINE catMaybes #-}
 
   filterA :: Applicative f => (a -> f Bool) -> t a -> f (t a)
-  filterA f = wither (\a -> (\b -> if b then Just a else Nothing) <$> f a)
+  filterA = filterAOf wither
 
   filter :: (a -> Bool) -> t a -> t a
-  filter f = runIdentity . filterA (Identity . f)
+  filter = filterOf wither
   {-# INLINE filter #-}
 #if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ >= 707
   {-# MINIMAL wither | mapMaybe | catMaybes #-}
@@ -82,28 +113,40 @@ blightM :: (Monad m, Witherable t) => t a -> (a -> MaybeT m b) -> m (t b)
 blightM = flip witherM
 {-# INLINE blightM #-}
 
--- | Removes duplicate elements from a list, keeping only the first
---   occurrence. This is exponentially quicker than using
---   'Data.List.nub' from 'Data.List'.
-ordNub :: (Witherable t, Ord a) => t a -> t a
-ordNub t = evalState (filterA f t) Set.empty
+-- | Remove the duplicate elements through a filter.
+ordNubOf :: Ord a => FilterLike' (State (Set.Set a)) s a -> s -> s
+ordNubOf w t = evalState (filterAOf w f t) Set.empty
   where
     f a = state $ \s ->
       case Set.member a s of
         True  -> (False, s)
         False -> (True, Set.insert a s)
-{-# INLINE ordNub #-}
+{-# INLINE ordNubOf #-}
 
--- | Removes duplicate elements from a list, keeping only the first
---   occurrence. This is usually faster than 'ordNub', especially for
---   things that have a slow comparion (like 'String')
-hashNub :: (Witherable t, Eq a, Hashable a) => t a -> t a
-hashNub t = evalState (filterA f t) HSet.empty
+-- | Remove the duplicate elements through a filter.
+-- It is often faster than 'ordNubOf', especially when the comparison is expensive.
+hashNubOf :: (Eq a, Hashable a) => FilterLike' (State (HSet.HashSet a)) s a -> s -> s
+hashNubOf w t = evalState (filterAOf w f t) HSet.empty
   where
     f a = state $ \s ->
       case HSet.member a s of
         True  -> (False, s)
         False -> (True, HSet.insert a s)
+{-# INLINE hashNubOf #-}
+
+-- | Removes duplicate elements from a list, keeping only the first
+--   occurrence. This is exponentially quicker than using
+--   'Data.List.nub' from 'Data.List'.
+ordNub :: (Witherable t, Ord a) => t a -> t a
+ordNub = ordNubOf wither
+{-# INLINE ordNub #-}
+
+-- | Removes duplicate elements from a list, keeping only the first
+--   occurrence. This is usually faster than 'ordNub', especially for
+--   things that have a slow comparion (like 'String')
+-- hashNubOf :: (Witherable t, Eq a, Hashable a) => t a -> t a
+hashNub :: (Witherable t, Eq a, Hashable a) => t a -> t a
+hashNub = hashNubOf wither
 {-# INLINE hashNub #-}
 
 instance Witherable Maybe where
